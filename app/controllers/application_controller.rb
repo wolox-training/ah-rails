bb79e65e33c59d398bbb500cb7bd9ac361b9a51e
Alfonso Henriquez <alfo704@gmail.com>
2018-05-25 14:30:41 -0400

    Removes commentary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name registration])
-    # devise_parameter_sanitizer.for(:sign_up) { |u|
-    #     u.permit(:first_name, :last_name, :registration)
-    # }
   end
 end
